7cfb1cf56d0ef1d31b0517dd980b989bddb2679c
baladasoji <bala.dasoji@gmail.com>
2018-09-06 17:21:25 +0200

    New modifications to support roles etc

--- server.js
+++ server.js
@@ -10,16 +10,39 @@ const HOST = '0.0.0.0';
 const app = express();
 
 // App
-app.get('/', (req, res) => {
+//
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req,res,next) {
   res.send('Hello world\n');
 });
 
-app.post('/token', function(req, res) {
+app.post('/roles', function(req, res, next) {
     var auth_header = req.header('Authorization');
     var token = auth_header.substring(7);
     var decoded = jwt.decode(token);
-    res.send(decoded.roles);
+    
+    if ('roles' in decoded )
+    {
+    	res.send(decoded.roles);
+    }
+    else
+    {
+	res.send('[]');
+    }
 });
 
+app.post('/username', function(req, res, next) {
+    var auth_header = req.header('Authorization');
+    var token = auth_header.substring(7);
+    var decoded = jwt.decode(token);
+    res.send(decoded.upn);
+});
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
